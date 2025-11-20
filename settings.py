b459cec8aa3169d28b69d8d4a4282998d6e9d17f
TheGrittyDev <thegrittydev@gmail.com>
2025-11-20 16:35:26 +0530

    Settings Class

---
+++ settings.py
@@ -0,0 +1,10 @@
+class Settings:
+    """A class to store all the settings for the game."""
+
+    def __init__(self):
+        """Initialize the game's settings."""
+
+        # Screen settings:
+        self.screen_width = 1200
+        self.sreen_height = 800
+        self.bg_color = (230, 230, 230)
