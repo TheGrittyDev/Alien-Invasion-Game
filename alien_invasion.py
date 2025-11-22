6a8aab59fac8c3d062edd7cba2ca3bc5f6acb2a4
TheGrittyDev <thegrittydev@gmail.com>
2025-11-22 20:09:00 +0530

    Limited bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -28,7 +28,7 @@ class AlienInvasion:
         while True:
             self._check_events()   # Watch for keyboard and mouse events.
             self.ship.update()     # Update ship's position 
-            self.bullets.update()  # Update bullets' postion     
+            self._update_bullets() # Update bullets' postion 
             self._update_screen()  # Redraw the screen during each pass through the loop.           
             self.clock.tick(60)
 
@@ -81,8 +81,20 @@ class AlienInvasion:
 
     def _fire_bullet(self):
         """Create a new bullet and add it to the bullets group."""
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        """update bullets' positions and delete old bullets"""
+        # Update bullets' positions
+        self.bullets.update()
+        # Get rid of bullets that have disappeared.
+        # Looping over a copy instead of original list bcoz python 
+        # expects the list length to be same while the loop is still running
+        for bullet in self.bullets.copy():       
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet) 
 
 if __name__ == '__main__':
     # Make a game instance, and run the game.
