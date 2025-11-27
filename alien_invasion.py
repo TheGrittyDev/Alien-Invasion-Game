2aa5ff42baa9590371ac8e1f1adfea86dfc7a56e
TheGrittyDev <thegrittydev@gmail.com>
2025-11-27 10:06:01 +0530

    alien movement

--- alien_invasion.py
+++ alien_invasion.py
@@ -32,6 +32,7 @@ class AlienInvasion:
             self._check_events()   # Watch for keyboard and mouse events.
             self.ship.update()     # Update ship's position 
             self._update_bullets() # Update bullets' postion
+            self._update_aliens()  # Update aliens' position
             self._update_screen()  # Redraw the screen during each pass through the loop.           
             self.clock.tick(60)
 
@@ -100,6 +101,11 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet) 
 
+    def _update_aliens(self):
+        """Update all alien positions"""
+        self._check_fleet_edges()
+        self.aliens.update()
+
     def _create_fleet(self):
         """Create the fleet of aliens."""
         # Create an alien and keep adding aliens until there's no room left.
@@ -126,6 +132,19 @@ class AlienInvasion:
         new_alien.rect.x = x_position
         new_alien.rect.y = y_position
         self.aliens.add(new_alien)
+
+    def _check_fleet_edges(self):
+        """Respond appropriately if any aliens have reached an edged"""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+    def _change_fleet_direction(self):
+        """Drop the entire fleet and change the fleet's direction."""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
     
 
 if __name__ == '__main__':
