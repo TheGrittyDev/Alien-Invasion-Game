9718ed90dd227f69ced3c1e7cb659a37dc787942
TheGrittyDev <thegrittydev@gmail.com>
2025-11-27 10:19:25 +0530

    Shooting down aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -101,6 +101,11 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet) 
 
+        # Check for any bullets that have hit aliens.
+        # If so, get rid of the bullet and the alien.
+        collisions  = pygame.sprite.groupcollide(
+                self.bullets, self.aliens, True, True)
+
     def _update_aliens(self):
         """Update all alien positions"""
         self._check_fleet_edges()
